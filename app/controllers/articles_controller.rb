5438d8cd981d835bc7a3e3fc1b6c64830d61f984
fortpol <rafaelofil@outlook.com>
2023-05-04 13:19:33 -0300

    add 'new' and 'create' to articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,18 @@ class ArticlesController < ApplicationController
   def show
     @articles = Article.find(params[:id])
   end
+
+  def new
+    @articles = Article.new
+  end
+
+  def create
+    @articles = Article.new(title: "...", body: "...")
+
+    if @articles.save
+      redirect_to @articles
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
