cc605c217e1762f9d75bff5b09e91799898b45ab
fortpol <rafaelofil@outlook.com>
2023-05-18 21:54:23 -0300

    concern reusability of code

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,6 +7,6 @@ class CommentsController < ApplicationController
 
   private
   def comments_params
-    params.require(:comment).permit(:commenter, :body)
+    params.require(:comment).permit(:commenter, :body, :status)
   end
 end
